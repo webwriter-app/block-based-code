4152acb88cb54bb1715a34a3b8263acd16d5b833
Philipp Lentzen <code@philipplentzen.dev>
2024-06-16 20:26:11 +0200

    Inits locales
    #7

---
+++ widgets/locales/de.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/no-default-export
+export default {
+  start: "Starte Ausführung",
+  stop: "Stoppe Ausführung",
+  fullscreen: "Vollbildmodus",
+  fullscreenExit: "Vollbildmodus verlassen",
+};
---
+++ widgets/locales/en.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/no-default-export
+export default {
+  start: "Start Execution",
+  stop: "Stop Execution",
+  fullscreen: "Enter Fullscreen",
+  fullscreenExit: "Leave Fullscreen",
+};
