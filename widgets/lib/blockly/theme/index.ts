4b129cf41c27b0342f0acded5e36ba793bce1e8f
Philipp Lentzen <code@philipplentzen.dev>
2024-06-20 19:49:29 +0200

    Adds scratch colors
    #12

--- widgets/lib/blockly/theme/index.ts
+++ widgets/lib/blockly/theme/index.ts
@@ -1,12 +1,42 @@
 import * as Blockly from "blockly";
 
+// Heavily inspired by Scratch Colors.
+// https://github.com/scratchfoundation/scratch-blocks/wiki/Colors
+
 const categoryStyles = {
-  events_category: { colour: "#FFBF00" },
+  controls_category: { colour: "#ffab19" },
+  events_category: { colour: "#ffbf00" },
+  motions_category: { colour: "#4c97ff" },
+  operators_category: { colour: "#59c059" },
+};
+
+const blockStyles = {
+  control_blocks: {
+    colourPrimary: "#ffab19",
+    colourSecondary: "#ec9c13",
+    colourTertiary: "#cf8b17",
+  },
+  event_blocks: {
+    colourPrimary: "#ffbf00",
+    colourSecondary: "#e6ac00",
+    colourTertiary: "#cc9900",
+  },
+  motion_blocks: {
+    colourPrimary: "#4c97ff",
+    colourSecondary: "#4280d7",
+    colourTertiary: "#3373cc",
+  },
+  operator_blocks: {
+    colourPrimary: "#59c059",
+    colourSecondary: "#46b946",
+    colourTertiary: "#389438",
+  },
 };
 
 Blockly.Theme.defineTheme("webwriter", {
   name: "webwriter",
   base: Blockly.Themes.Zelos,
   categoryStyles,
+  blockStyles,
   startHats: true,
 });
